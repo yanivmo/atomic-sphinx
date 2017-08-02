63536c532578dbb479b95102185ba59dd16d8036
Yaniv Mordekhay <y.mordekhay@gmail.com>
2017-08-02 07:07:37 +0300

    Reset the project by importing boilerplate code
    Overwritten all the existing files with the boilerplate from
    https://github.com/colinskow/angular-electron-dream-starter

--- src/renderer.js
+++
@@ -1,8 +0,0 @@
-/* eslint-env browser */
-const path = require('path');
-const remote = require('electron').remote;
-
-const activeProject = remote.getGlobal('activeProject');
-const webview = document.getElementById('content');
-
-webview.setAttribute('src', path.join(activeProject.contentDir, activeProject.homePage));
